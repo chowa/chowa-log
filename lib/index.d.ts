04a1e2f417c5eb26979a132db16ff72e4cf81f87
smoking rabbit <xuecong8226@gmail.com>
2020-01-13 20:34:08 +0800

    Chowa logs

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+import * as colors from 'colors/safe';
+
+export declare function success(msg: string): void;
+
+export declare function warning(msg: string): void;
+
+export declare function error(msg: string): void;
+
+export declare function info(msg: string): void;
+
+export default info;
